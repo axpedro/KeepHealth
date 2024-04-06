dafbc365fb7001069a3cf6c77846b22907b57825
axpedro <pedruax@hotmail.com>
2024-04-06 19:50:37 -0300

    Ex-4: INserido informacoes no localstorage, validacao usuario e senha, navigate para home e cadastro

--- keepHealth/src/app/cadastro/cadastro.component.ts
+++ keepHealth/src/app/cadastro/cadastro.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
+import { LoginComponent } from '../login/login.component';
 
 @Component({
   selector: 'app-cadastro',
   standalone: true,
-  imports: [],
+  imports: [LoginComponent],
   templateUrl: './cadastro.component.html',
   styleUrl: './cadastro.component.css'
 })
--- keepHealth/src/app/login/login.component.ts
+++ keepHealth/src/app/login/login.component.ts
@@ -1,12 +1,52 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { Router } from '@angular/router';
+import { HomeComponent } from '../home/home.component';
+import { CadastroComponent } from '../cadastro/cadastro.component';
 
 @Component({
   selector: 'app-login',
   standalone: true,
-  imports: [],
+  imports: [CommonModule, FormsModule, HomeComponent, CadastroComponent],
   templateUrl: './login.component.html',
   styleUrl: './login.component.css'
 })
 export class LoginComponent {
+  constructor(private router: Router) {
+    
+   }
+
+  inputDoemail ='';
+  inputDaSenha ='';
+  
+usuarios = [{
+usuario:"pedro",
+senha:"a1b2c4d4"
+}]
+
+
+ngOnInit() {
+  const usuariosString = JSON.stringify(this.usuarios);
+  localStorage.setItem("listaUsuarios", usuariosString);
+}
+
+verifica(){
+  const usuarioLista = JSON.parse(localStorage.getItem("listaUsuarios") || '[]');
+  if(this.inputDoemail === usuarioLista[0].usuario && this.inputDaSenha === usuarioLista[0].senha){
+
+//alert('login realizado com sucesso')
+this.router.navigate(['/home']);
+  }
+  else 
+  alert('Usuário ou senha inválidos')
+}
+redefineSenha(){
+  alert('Sua senha foi alterada para: "a1b2c4d4". Utilize a nova senha para logar')
+}
+
+Cadastrar(){
+  this.router.navigate(['/cadastro']);
+}
 
 }
