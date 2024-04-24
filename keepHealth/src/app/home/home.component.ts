e0f3c3bff4c92adcfde0b8d86c540f1be263eada
axpedro <pedruax@hotmail.com>
2024-04-24 18:03:16 -0300

    Exercicio 5 - Home - Criado o dialog com Ngprime select/datepicker e salvamento no localstorage

--- keepHealth/src/app/home/home.component.ts
+++ keepHealth/src/app/home/home.component.ts
@@ -1,46 +1,80 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { LoginComponent } from '../login/login.component';
 import { Dialog, DialogModule } from 'primeng/dialog';
 import { ButtonModule } from 'primeng/button';
-
 import { FormsModule, NgModel } from '@angular/forms';
 import { ToastModule } from 'primeng/toast';
 import { MessageService } from 'primeng/api';
+import { DropdownModule } from 'primeng/dropdown';
+import { CommonModule } from '@angular/common';
+import { CalendarModule } from 'primeng/calendar';
 
-
+interface Atividade {
+  name: string;
+  code: string;
+}
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [LoginComponent, FormsModule, ButtonModule, DialogModule, ToastModule ],
+  imports: [
+    LoginComponent,
+    FormsModule,
+    ButtonModule,
+    ToastModule,
+    CommonModule,
+    DropdownModule,
+    DialogModule,
+    CalendarModule,
+  ],
   templateUrl: './home.component.html',
   styleUrl: './home.component.css',
   providers: [MessageService],
 })
-export class HomeComponent {
-  constructor(private messageService: MessageService){}
+export class HomeComponent implements OnInit {
+  constructor(private messageService: MessageService) {}
   visible: boolean = false;
-  
-  listaUsuarios: any ;
-  ngOnInit(){
+  date: Date | undefined;
+  listaUsuarios: any;
 
+  atividades: Atividade[] | undefined;
+  listaAtividades:
+    | [
+        {
+          ativ: '';
+          data: '';
+        }
+      ]
+    | undefined;
+  atividadeSelecionada: Atividade | undefined;
+
+  ngOnInit() {
     const localData = localStorage.getItem('listaUsuarios');
-    if(localData != null){
+    if (localData != null) {
       this.listaUsuarios = JSON.parse(localData);
       //console.log(this.listaUsuarios)
-      
+
       //this.listaAlimentosCompleta = this.listaAlimentosrec;
-  
-  
+    }
+
+    this.atividades = [
+      { name: 'Correr', code: 'correr' },
+      { name: 'Academia', code: 'academia' },
+      { name: 'Natação', code: 'natacao' },
+      { name: 'Caminhada', code: 'caminhada' },
+      { name: 'Volei', code: 'volei' },
+    ];
   }
 
-  }
-  show() {
-    this.messageService.add({ severity: 'success', summary: 'Success', detail: 'Message Content' });
-}
-  showAtividade(){
+  showDialog() {
     this.visible = true;
   }
-
-
-
+  salvarLocal() {
+    console.log(this.atividadeSelecionada);
+    const listaAtividadesObj = {
+      ativ: this.atividadeSelecionada?.code,
+      data: this.date,
+    };
+    const ListaAtividadesString = JSON.stringify(listaAtividadesObj);
+    localStorage.setItem('atividades', ListaAtividadesString);
+  }
 }
