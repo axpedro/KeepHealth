5a8ae6a03ff0f279386f099c47ca3a0c11abf696
axpedro <pedruax@hotmail.com>
2024-04-24 11:31:34 -0300

    TEntando implementar o dialog com ngprime

--- keepHealth/src/app/home/home.component.ts
+++ keepHealth/src/app/home/home.component.ts
@@ -1,14 +1,25 @@
 import { Component } from '@angular/core';
 import { LoginComponent } from '../login/login.component';
+import { Dialog, DialogModule } from 'primeng/dialog';
+import { ButtonModule } from 'primeng/button';
+
+import { FormsModule, NgModel } from '@angular/forms';
+import { ToastModule } from 'primeng/toast';
+import { MessageService } from 'primeng/api';
+
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [LoginComponent],
+  imports: [LoginComponent, FormsModule, ButtonModule, DialogModule, ToastModule ],
   templateUrl: './home.component.html',
-  styleUrl: './home.component.css'
+  styleUrl: './home.component.css',
+  providers: [MessageService],
 })
 export class HomeComponent {
+  constructor(private messageService: MessageService){}
+  visible: boolean = false;
+  
   listaUsuarios: any ;
   ngOnInit(){
 
@@ -23,5 +34,13 @@ export class HomeComponent {
   }
 
   }
+  show() {
+    this.messageService.add({ severity: 'success', summary: 'Success', detail: 'Message Content' });
+}
+  showAtividade(){
+    this.visible = true;
+  }
+
+
 
 }
